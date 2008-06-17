c744f6f9417719a90df2630607af2d8bf05fefca
Richard McGuire <rickmcguire@apache.org>
2008-06-17 11:04:26 +0000

    GERONIMO-4120 Sun API specification information appears directly in some of the javamail classes.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/javamail/trunk@668614 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-javamail_1.4/geronimo-javamail_1.4_provider/src/main/java/org/apache/geronimo/javamail/store/imap/Rights.java
+++ geronimo-javamail_1.4/geronimo-javamail_1.4_provider/src/main/java/org/apache/geronimo/javamail/store/imap/Rights.java
@@ -284,36 +284,14 @@ public class Rights implements Cloneable {
     
     class RightComparator implements Comparator {
         /**
-         * Compares its two arguments for order.  Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.<p>
-         *
-         * The implementor must ensure that <tt>sgn(compare(x, y)) ==
-         * -sgn(compare(y, x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-         * implies that <tt>compare(x, y)</tt> must throw an exception if and only
-         * if <tt>compare(y, x)</tt> throws an exception.)<p>
-         *
-         * The implementor must also ensure that the relation is transitive:
-         * <tt>((compare(x, y)&gt;0) &amp;&amp; (compare(y, z)&gt;0))</tt> implies
-         * <tt>compare(x, z)&gt;0</tt>.<p>
-         *
-         * Finally, the implementer must ensure that <tt>compare(x, y)==0</tt>
-         * implies that <tt>sgn(compare(x, z))==sgn(compare(y, z))</tt> for all
-         * <tt>z</tt>.<p>
-         *
-         * It is generally the case, but <i>not</i> strictly required that
-         * <tt>(compare(x, y)==0) == (x.equals(y))</tt>.  Generally speaking,
-         * any comparator that violates this condition should clearly indicate
-         * this fact.  The recommended language is "Note: this comparator
-         * imposes orderings that are inconsistent with equals."
-         *
-         * @param o1 the first object to be compared.
-         * @param o2 the second object to be compared.
-         * @return a negative integer, zero, or a positive integer as the
-         * 	       first argument is less than, equal to, or greater than the
-         *	       second.
-         * @throws ClassCastException if the arguments' types prevent them from
-         * 	       being compared by this Comparator.
+         * Perform a sort comparison to order two Right objects.
+         * The sort is performed using the string value. 
+         * 
+         * @param o1     The left comparator
+         * @param o2     The right comparator.
+         * 
+         * @return 0 if the two items have equal ordering, -1 if the 
+         *         left item is lower, 1 if the left item is greater.
          */
         public int compare(Object o1, Object o2) {
             // compare on the string value 
