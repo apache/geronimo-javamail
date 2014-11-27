6edb2dc64c7a807a6357db38844ae063fd806b09
Hendrik Saly <salyh@apache.org>
2014-11-27 20:31:40 +0000

    Imports fixed (they break Java 6 build)
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/javamail/trunk/geronimo-javamail_1.4@1642228 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-javamail_1.4_provider/src/test/java/org/apache/geronimo/javamail/issues/IssuesTest.java
+++ geronimo-javamail_1.4_provider/src/test/java/org/apache/geronimo/javamail/issues/IssuesTest.java
@@ -33,7 +33,6 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMessage.RecipientType;
 import javax.mail.internet.MimeMultipart;
 
 import junit.framework.Assert;
